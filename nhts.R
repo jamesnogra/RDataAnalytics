4599c749fafc8e612dc15aa9988fe40790393f8d
jamesnogra <love.gerald@yahoo.com>
2018-03-25 20:34:54 +0800

    Added a total in the houses csv.

--- nhts.R
+++ nhts.R
@@ -2,5 +2,6 @@ nhts <- read.csv("C:/Users/James/Desktop/DataMiningFinalExam/DataMiningFinalExam
 nhts <- na.omit(nhts)
 #View(nhts)
 
-results <- kmeans(nhts, 4)
-plot(nhts[c("single_house", "duplex", "apartment_or_condominiums_or_townhouses", "commercial_or_industrial_or_agricultural_building_or_house", "other_housing_cave_or_boat_or_under_a_bridge_etc.")], col=results$cluster)
+results <- kmeans(nhts, 2)
+#plot(nhts[c("single_house", "duplex", "apartment_or_condominiums_or_townhouses", "commercial_or_industrial_or_agricultural_building_or_house", "other_housing_cave_or_boat_or_under_a_bridge_etc.")], col=results$cluster)
+plot(nhts[c("single_house", "duplex", "apartment_or_condominiums_or_townhouses")], col=results$cluster)
